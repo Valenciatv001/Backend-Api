a664b7902e7b91b0fa9f4ec5499e84b74adb9a01
Valenciatv001 <67506940+Valenciatv001@users.noreply.github.com>
2023-09-19 16:46:54 +0100

    node express

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const connectDb = require('./config/dbConnection')
+const errorHandler = require('./middleware/errorHandler')
+const dotenv = require('dotenv').config()
+
+
+async function startApp() {
+    await connectDb();
+  }
+  
+  startApp();
+const app = express()
+
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use('/api/contacts', require('./routes/contactRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
+
+
+
+
